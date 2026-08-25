8981115c72d2993287f33c5eaaeff8f7adf12d17
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-25 22:41:20 +0530

    dffa

---
+++ Collection-Map/src/com/collection/framework/hashmap/intro/HashMapExample.java
@@ -0,0 +1,31 @@
+package com.collection.framework.hashmap.intro;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		
+		HashMap<String , String>  map = new HashMap<String, String>();
+
+		
+		map.put("Karnataka", "bengaluru");
+		map.put("gujrat", "gandhi nagar");
+		map.put("nagaland", "kohima");
+		map.put("panjab", "chandigarh");
+		map.put("haryana", "chandigarh");
+		
+		System.out.println(map);
+		
+		Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
+		while(itr.hasNext()) {
+		Map.Entry<String, String>	entry = itr.next();
+		
+		System.out.println(entry);
+			
+		}
+	}
+
+}
